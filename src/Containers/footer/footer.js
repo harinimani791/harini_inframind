17fd6e472fb21a3f0fccc2c234563c1986c8fae7
Harini <harinimani791@gmail.com>
2021-02-13 17:18:39 +0530

    final commit

---
+++ src/Containers/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SocialMediaHorizontal from "../SocialMedia.js/SocialMediaHorizontal/SocialMediaHorizontal";
+import "./footer.css";
+
+function footer() {
+  return (
+    <>
+      <h5 className="text-center copyright">© Copyrights reserved by Harini</h5>
+    </>
+  );
+}
+export default footer;
